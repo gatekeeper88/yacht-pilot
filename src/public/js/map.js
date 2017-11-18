3fed162435499418ca0ae761b960ad3e16ef9554
Alex <aleksandarristov@Alexs-MacBook-Air.local>
2017-11-18 11:21:51 -0600

    cleanup

--- src/public/js/map.js
+++ src/public/js/map.js
@@ -1,5 +1,5 @@
 
-var app = angular.module('leafletApp', ['ngResource', 'leaflet-directive']);
+var app = angular.module('leafvarApp', ['ngResource', 'leafvar-directive']);
 
 app.service('UtilService', function($http) {
 	this.extractResponseBody = function(res) {
@@ -38,7 +38,7 @@ app.service('PlaceSearchService', function() {
         		+ placename + '&orderby=relevance&maxRows=10&username=yachtpilot&callback=JSON_CALLBACK')
             .then(function(response) {
             	if (!response || response.data) return [];
-                let places = response.data.geonames.map(function(location) {
+                var places = response.data.geonames.map(function(location) {
                     return {
                         title: location.name + ', ' + location.countryName,
                         type: 'location',
@@ -57,7 +57,7 @@ app.service('PlaceSearchService', function() {
 app.service('WeatherService', function($http, UtilService) {
  	this.getWeatherForecast = function(position) {
 e;
- 		let coords = {
+ 		var coords = {
  			lat: position.coords.latitude,
  			lon: position.coords.longitude,
  		};
@@ -72,18 +72,16 @@ e;
 });
 
 app.controller('CenterController', ['$scope',
-		'leafletData', '$http', 'MapApiService',
+		'leafvarData', '$http', 'MapApiService',
 		'LocationService', 'WeatherService', '$compile',
 		'$templateRequest', '$sce', '$timeout',
 	function($scope,
-		leafletData, $http, MapApiService,
+		leafvarData, $http, MapApiService,
 		LocationService, WeatherService, $compile,
 		$templateRequest, $sce, $timeout) {
 	initController();
 
 	$scope.selectedMarker;
-
-	$scope.name = 'Alex';
 
 	$scope.setSelectedMarker = function(marker) {
 		$scope.selectedMarker = marker;
@@ -99,7 +97,6 @@ app.controller('CenterController', ['$scope',
 	};
 
 	$scope.toggleEditing = function() {
-		console.log('Toggling ...');
 		marker = $scope.getSelectedMarker();
 		marker.editing = !marker.editing;
 	};
@@ -109,7 +106,7 @@ app.controller('CenterController', ['$scope',
 	}
 
 	function generateMarkerId() {
-		let index = $scope.markers.length;
+		var index = $scope.markers.length;
 		if (index == 0) return index; else return index++;
 	}
 
@@ -138,7 +135,7 @@ app.controller('CenterController', ['$scope',
 		}
 
 		function displayWeatherForecast(forecast) {
-			let timeZone = 1000 * (new Date().getTimezoneOffset())*(-60);
+			var timeZone = 1000 * (new Date().getTimezoneOffset())*(-60);
 			if (!forecast) return;
 				forecast.list = forecast.list.slice(0, 5);
 			forecast.list.forEach(function(day) {
@@ -157,10 +154,10 @@ app.controller('CenterController', ['$scope',
 
 		function drawRoute(route) {
 			if (!route || !route.points) return;
-			let latLngs = route.points.map(function(point) {
+			var latLngs = route.points.map(function(point) {
 			 	return L.latLng(point.lat, point.lng);
 				});
-			let polyline = L.polyline(latLngs, {color: 'black'});
+			var polyline = L.polyline(latLngs, {color: 'black'});
 			polyline.addTo($scope.map);
 			MapEventHandlers.onPolylineDrawn(polyline);
 		}
@@ -172,9 +169,9 @@ app.controller('CenterController', ['$scope',
 
 	function initMapDrawControl(map) {
 		$scope.map = map;
-		let drawnItems = new L.FeatureGroup();
+		var drawnItems = new L.FeatureGroup();
 		map.addLayer(drawnItems);
-		let drawControl = new L.Control.Draw({
+		var drawControl = new L.Control.Draw({
 		    edit: {
 		        featureGroup: drawnItems,
 		    },
@@ -198,16 +195,16 @@ app.controller('CenterController', ['$scope',
 	        },
     	});
 
-  		leafletData.getMap().then(onMapDrawn);
+  		leafvarData.getMap().then(onMapDrawn);
 	}
 
 	var MapEventHandlers = {};
 
 	function initMapEventHandlers(map) {
-		let onPolylineDrawn = function(layer) {
-			let index = $scope.routes.length;
-			let points = layer._latlngs;
-			let newRoute = {
+		var onPolylineDrawn = function(layer) {
+			var index = $scope.routes.length;
+			var points = layer._latlngs;
+			var newRoute = {
 				id: index++,
 				points: points,
 				layer: layer,
@@ -216,26 +213,26 @@ app.controller('CenterController', ['$scope',
 		};	
 		MapEventHandlers.onPolylineDrawn = onPolylineDrawn;
 
-		let onMarkerDrawn = function(layer) {
-			let id = generateMarkerId();
+		var onMarkerDrawn = function(layer) {
+			var id = generateMarkerId();
 			layer.id = id;
 
-			let marker = {
+			var marker = {
 				id: id,
 				position: layer.getLatLng(),
 				layer: layer,
 			};
 
-			let templateUrl = $sce.getTrustedResourceUrl('popup-template.html');
+			var templateUrl = $sce.getTrustedResourceUrl('popup-template.html');
 			$templateRequest(templateUrl).then(onTemplateLoaded);
 
 			function onTemplateLoaded(template) {
 				$timeout(function() {
 					marker.editing = true;
 					$scope.setSelectedMarker(marker);
-					let templateEl = angular.element(template);
-					let linkFn = $compile(templateEl);
- 					let element = linkFn($scope);
+					var templateEl = angular.element(template);
+					var linkFn = $compile(templateEl);
+ 					var element = linkFn($scope);
  					$scope.$apply();
 					layer.bindPopup(element.html());
 					layer.openPopup();
@@ -248,14 +245,14 @@ app.controller('CenterController', ['$scope',
 
 
 		function onMarkerClick(e) {
-			let id = e.target.id;
-			let marker = $scope.markersCache[id];
+			var id = e.target.id;
+			var marker = $scope.markersCache[id];
 			if (!marker) return;
 			$scope.setSelectedMarker(marker);
 		}
 
 		function onDrawCreated(e) {
-		    let type = e.layerType,
+		    var type = e.layerType,
 		        layer = e.layer;
 
 		    if (type === 'marker') {
@@ -269,37 +266,35 @@ app.controller('CenterController', ['$scope',
 	}
 
 	$scope.showRoute = function(index) {
-		let route = $scope.routes[index];
-		let bounds = route.layer.getBounds();
+		var route = $scope.routes[index];
+		var bounds = route.layer.getBounds();
 		$scope.map.fitBounds(bounds);
 	};
 
 	$scope.removeRoute = function(index) {
-		console.log('index', index);
-		let route = $scope.routes[index];
+		var route = $scope.routes[index];
 		$scope.map.removeLayer(route.layer);
 	};
 
 	$scope.saveRoute = function(index) {
-		let route = $scope.routes[index];
-		let routeToSave = {
+		var route = $scope.routes[index];
+		var routeToSave = {
 			points: route.points,
 		};
 		MapService.saveRoute(routeToSave);
 	};
 
 	$scope.showMarker = function(index) {
-		let marker = $scope.markersCache[index];
-		console.log('Got marker', marker, index);
+		var marker = $scope.markersCache[index];
 		if (!marker) return;
 		$scope.map.panTo(marker.layer.getLatLng());
 	};
 
 	$scope.removeMarker = function(index) {
-
+		// TODO: remove marker
 	};
 
 	$scope.saveMarker = function(index) {
-
+		//TODO: save marker
 	};
 }]);
